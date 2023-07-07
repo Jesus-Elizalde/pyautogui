0277ace2dfac5ebf0e8c9606216cd3d9f34fcf1f
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-07-06 23:02:40 -0700

    opens new tab and search youtube

--- pyautogui_imp.py
+++ pyautogui_imp.py
@@ -1,5 +1,13 @@
 import pyautogui
+import time
 
-res = pyautogui.locateOnScreen("edit.png")
-print(res)
-print(pyautogui.center(res))
+# delay for 1 second
+time.sleep(1)
+
+
+# Opens a new tab
+pyautogui.hotkey("command","t")
+
+# Search youtube
+pyautogui.write("https://www.youtube.com/")
+pyautogui.hotkey("enter")
